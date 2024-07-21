12e37d5f3a0e3f9b103c42b70e6a6465a94b5228
UsikovaVA <9004847432m@gmail.com>
2024-07-21 21:17:26 +0300

    изменила разметку, поправила стили и написала правила для модального окна

---
+++ index.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const buttons = [
+        { id: 'navBtn1', imageId: 'btnDumbbel', src: 'img/dumbbell.svg', srcWhite: 'img/dumbbell-white.svg' },
+        { id: 'navBtn2', imageId: 'btnWater', src: 'img/water.svg', srcWhite: 'img/water-white.svg' },
+        { id: 'navBtn3', imageId: 'btnEat', src: 'img/eat.svg', srcWhite: 'img/eat-white.svg' },
+        { id: 'navBtn4', imageId: 'btnAdd', src: 'img/add.svg', srcWhite: 'img/add-white.svg' },
+        { id: 'navBtn5', imageId: 'Dumbbel', src: 'img/dumbbell.svg', srcWhite: 'img/dumbbell-white.svg' },
+        { id: 'navBtn6', imageId: 'Water', src: 'img/water.svg', srcWhite: 'img/water-white.svg' },
+        { id: 'navBtn7', imageId: 'Eat', src: 'img/eat.svg', srcWhite: 'img/eat-white.svg' }
+    ];
+
+    buttons.forEach(({ id, imageId, src, srcWhite }) => {
+        const button = document.getElementById(id);
+        const image = document.getElementById(imageId);
+        image.src = src;
+
+        button.addEventListener('click', () => {
+            if (image.src.includes(src)) {
+                image.src = srcWhite;
+                button.style.backgroundColor = '#5051f9';
+            } else {
+                image.src = src;
+                button.style.backgroundColor = '';
+            }
+        });
+    });
+    var dialog = document.querySelector('dialog')
+    // выводим окно
+    document.querySelector('#navBtn4').onclick = function () {
+      dialog.showModal()
+    }
+    // скрываем окно
+    document.querySelector('#close').onclick = function () {
+      dialog.close() 
+    }
+
+// продожи тут
+
+});
+
+
+
